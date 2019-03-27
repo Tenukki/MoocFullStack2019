98f501cc5f47820feb01e75392d56fb5b318aaf1
Tenukki <tenhunens@hotmail.com>
2019-03-27 15:26:50 +0200

    sfasfsf

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -6,15 +6,43 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [keski, setKeski] = useState([])
+  const [yht, setYht] = useState(0)
+  const [sumKeski, setSumKeski] = useState([])
+
 
   const klik = (a) =>{
-      if(a === "g")
+      setYht(yht + 1)
+      if(a === "g"){
         setGood(good + 1)
+        setKeski(keski.concat(1))
+        setSumKeski(sumKeski.concat(1))
+      } 
       else if(a === "n"){
         setNeutral(1+neutral)
+        setKeski(keski.concat(0))
       }else if(a === "b"){
         setBad(1+bad)
+        setKeski(keski.concat(-1))
       }
+  }
+
+  const keskiarvo = () => {
+    let summa = 0
+    keski.forEach(element => {
+        summa += element
+      });
+
+      return summa === 0 ? 0 : summa/yht 
+  }
+
+  const sumkeskiarvo = () => {
+    let summa = 0
+    sumKeski.forEach(element => {
+        summa += element
+      });
+
+      return summa === 0 ? 0 : summa/yht * 100
   }
 
   const Button = (probs) =>{
@@ -37,6 +65,9 @@ const App = () => {
         <p>hyvä {good} </p>
         <p>neutraali {neutral}</p>
         <p>huono {bad}</p>
+        <p>Yhteensä {yht}</p>
+        <p>Keskiarvo {keskiarvo()}</p>
+        <p>positiivisia {sumkeskiarvo()}  %</p>
       
     </div>
   )
