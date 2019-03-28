e3b2fc37735dc0514e6bfb1208704da251a6e868
Tenukki <tenhunens@hotmail.com>
2019-03-28 12:35:16 +0200

    kaikki tehty 2.5

---
+++ osa2/kurssitiedotStep6/src/components/Course.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const Header = (probs) =>{
+  return(
+      <div>
+          <h1>{probs.notes.name}</h1>
+      </div>
+  )
+}
+
+const Content = (probs) =>{
+
+  const rivi = () => probs.osa.parts.map(asia =>
+    <Part nimi = {asia.name} maara = {asia.exercises}/>
+  )
+  return (
+    <>
+      {rivi()}
+    </>
+  )
+}
+
+const Part = (probs) =>{
+  return(
+      <div>
+          <p>{probs.nimi} {probs.maara}</p>
+      </div>
+  )
+}
+
+const Total = (probs) =>{
+ const summa = probs.osa.parts.reduce((s,p) => s +p.exercises,0)
+  
+   return(
+       <div>
+           <p>Yhteensä {summa} tehtävää</p>
+       </div>
+   )
+}
+
+const Course = ({ note }) => {
+
+  const kurssi = note.map(elemnent =>
+      <>
+      <Header notes = {elemnent}/>
+      <Content osa = {elemnent}/>
+      <Total osa = {elemnent}/>
+      </>
+    )
+  return (
+    <>
+      {kurssi}
+    </>
+    
+  )
+}
+
+export default Course
