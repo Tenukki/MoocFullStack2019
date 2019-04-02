528d82e039b2388349c790645db6075e97df91e0
Tenukki <tenhunens@hotmail.com>
2019-04-02 13:31:39 +0300

    2.11 näkyy vain puhelinluettelo2.11 branchissa

--- osa2/puhelinluettelo/src/index.js
+++ osa2/puhelinluettelo/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
+///Tämä uusi muutos tulisi näkyä vain pujelinluettelo2.11 branchissa
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
