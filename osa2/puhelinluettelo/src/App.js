b3feb2ea6a26fc92adaf2126c19c1a10c35f5ee2
Tenukki <tenhunens@hotmail.com>
2019-04-02 13:59:38 +0300

    nyt on artoo

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Note = (props) => {
   return (
@@ -40,12 +40,7 @@ const Numbers = (props) => {
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Martti Tienari', number: '040-123456' },
-    { name: 'Arto Järvinen', number: '040-123456' },
-    { name: 'Lea Kutvonen', number: '040-123456' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newPuh, setNewPuh ] = useState('')
   const [ newRaja, setNewRaja ] = useState('')
@@ -96,6 +91,16 @@ const App = () => {
     a = false
     
   }
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
 
 
