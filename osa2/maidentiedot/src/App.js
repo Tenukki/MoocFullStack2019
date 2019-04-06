5e82ad7fab3dce32409f399ec8c58793ad3abbd8
Tenukki <tenhunens@hotmail.com>
2019-04-06 15:11:49 +0300

    maat toimii

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -2,41 +2,45 @@ import React, { Component,useState,useEffect } from 'react';
 import Connect from './connect'
 
 
-const Country = ({maaObject}) => {
+
+const Country = (props) => {
+  const maaObject = [...props.taulukko]
   return (
     <>
-      <h1>{maaObject.name}</h1>
-      <h1>""</h1>
-      <p>{maaObject.population}</p>
+      <h1>{maaObject[0].name}</h1>
+      <p>capital {maaObject[0].capital}</p>
+      <p>population {maaObject[0].population}</p>
       <h2>languages</h2>
-      <ul>
-      <Languages maaObject = {maaObject}/>
-      </ul>
-      <img src={maaObject.flag} alt="kuva"/>
+      <div>
+        <ul>
+          {maaObject[0].languages.map(kieli => <li key = {kieli.iso639_1}>{kieli.name}</li>)}
+        </ul>
+      </div>
+      <img src={maaObject[0].flag} alt="kuva"/>
     </>
   )
 }
 
-const Languages = ({maaObject}) => {
-    return (
-      maaObject.languages.map(element => {
-        return <li>{element.name}</li>
-      })
-    ) 
+
+const Print = ({maa}) => {
+  return (maa.map(asia => {
+    return <p key = {asia.name}>{asia.name}</p>
+  }))
 }
+
 
 
 const App = () => {
   const [name, setName] = useState("");
-  const [maat,setMaat] = useState([])
+  const [maat, setMaat] = useState([])
   
+  console.log(maat)
   useEffect(() => {
     Connect
       .getAll().then(objects => {
         console.log(objects)
         console.log('promise fulfilled')
         setMaat(objects)
-        console.log(maat)
       })
   }, [])
   
@@ -45,15 +49,33 @@ const App = () => {
     setName(event.target.value)
   }
 
-  const printCountries = () => {
-    console.log(maat)
-  }
+  const printCountries = (maat) => {
+      //filteröidään maat joissa esiintyy tietty merkkijono
+      const taul = maat.filter(object => object.name.toUpperCase().includes(name.toUpperCase(),0))
+      console.log(taul)
+      if(taul.length > 10){
+        return(
+          <p>Too many matches, specify another filter</p>
+        )
+      }else if(taul.length === 1){
+        //tässä vika????
+        return(
+        <Country taulukko={taul}/>
+        )
+      }else{
+        return (
+          <>
+            <Print maa = {taul}/>
+          </>
+        )
+      }
+    }
 
-  
 
   return (
     <div>
       find countries<input type="text" value = {name} onChange = {setSearch}/>
+      {printCountries(maat)}
     </div>
   )
 
