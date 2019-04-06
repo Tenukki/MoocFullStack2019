ebe8d931efc94debac1edf882ae27dadcf37ff53
Tenukki <tenhunens@hotmail.com>
2019-04-06 13:15:36 +0300

    countries

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -1,18 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component,useState,useEffect } from 'react';
+import Connect from './connect'
+
+
+const Country = ({maaObject}) => {
+  return (
+    <>
+      <h1>{maaObject.name}</h1>
+      <h1>""</h1>
+      <p>{maaObject.population}</p>
+      <h2>languages</h2>
+      <ul>
+      <Languages maaObject = {maaObject}/>
+      </ul>
+      <img src={maaObject.flag} alt="kuva"/>
+    </>
+  )
+}
+
+const Languages = ({maaObject}) => {
+    return (
+      maaObject.languages.map(element => {
+        return <li>{element.name}</li>
+      })
+    ) 
+}
+
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [maat,setMaat] = useState([])
+  
+  useEffect(() => {
+    Connect
+      .getAll().then(objects => {
+        console.log(objects)
+        console.log('promise fulfilled')
+        setMaat(objects)
+        console.log(maat)
+      })
+  }, [])
+  
+  const setSearch = (event) => {
+    console.log(name)
+    setName(event.target.value)
+  }
 
-
+  const printCountries = () => {
+    console.log(maat)
+  }
 
   
-  render() 
-    return (
-      <div className="App">
-        
-      </div>
-    );
+
+  return (
+    <div>
+      find countries<input type="text" value = {name} onChange = {setSearch}/>
+    </div>
+  )
+
 }
 
 export default App;
