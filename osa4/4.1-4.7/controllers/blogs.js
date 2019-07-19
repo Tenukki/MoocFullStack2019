d10cbabe345482472560a406089afc6a17bbd3fe
Tenukki <tenhunens@hotmail.com>
2019-07-19 15:16:39 +0300

    jeee

--- osa4/4.1-4.7/controllers/blogs.js
+++ osa4/4.1-4.7/controllers/blogs.js
@@ -27,13 +27,39 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
-  try {
-    const savedblog = await blog.save()
-    response.json(savedblog.toJSON())
-  } catch (error) {
-    next(error)
-  }
+  
+    try {
+      if(!request.body.hasOwnProperty('url') && !request.body.hasOwnProperty('title')){
+        response.status(400)
+      }else{
+
+      
+
+      if(!request.body.hasOwnProperty('likes')){
+        console.log(request.body)
+
+        let a = request.body
+        let uusi = {
+          title: a.title,
+          author: a.author,
+          url: a.url,
+          likes: 0
+        }
+        console.log(uusi)
+        
+        const newBlog = new Blog(uusi)
+        newBlog.save()
+        response.json(uusi)
+        }else{
+            console.log("täällä jossa on likes")
+            const blog = new Blog(request.body)
+            const savedblog = await blog.save()
+            response.json(savedblog.toJSON())
+        }
+    }
+    } catch (error) {
+      next(error)
+    }
   
 })
 
--- osa4/4.1-4.7/tests/blog_api.test.js
+++ osa4/4.1-4.7/tests/blog_api.test.js
@@ -6,20 +6,16 @@ const Blog = require("../models/blog")
 
 const blogs = [
     {
-      _id: "5a422a851b54a676234d17f7",
       title: "React patterns",
       author: "Michael Chan",
       url: "https://reactpatterns.com/",
       likes: 7,
-      __v: 0
     },
     {
-      _id: "5a422aa71b54a676234d17f8",
       title: "Go To Statement Considered Harmful",
       author: "Edsger W. Dijkstra",
       url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
       likes: 5,
-      __v: 0
     } 
   ]
 
@@ -58,6 +54,69 @@ describe("Therea are 2 notes and they rae json", () => {
     })
 })
 
+test('One more blog was added to the database ', async () => {
+    const newblogs = {
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        likes: 0,
+    }
+
+    await api
+        .post("/api/blogs")
+        .send(newblogs)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    const blogss = await Blog.find({})
+    blogs2 = blogss.map(blogi => blogi.toJSON())
+    //console.log(blogs2)
+
+    expect(blogs2.length).toBe(blogs.length+1)
+})
+
+test('if likes is empty the number will be 0', async () => {
+    testblog = {
+        title: "Type wars",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    } 
+    const response = await api
+    .post("/api/blogs")
+    .send(testblog)
+    console.log(response.body)
+
+    expect(response.body.likes).toBe(0)
+})
+
+test('if status is 400', async () => {
+    testblog = {
+        author: "Type wars",
+    } 
+    const response = await api
+    .post("/api/blogs")
+    .send(testblog)
+    
+
+    expect(response.status).toBe(400)
+})
+
+/*
+test('Does not have field title or url', async () => {
+    testblog3 = {
+        _id: "5a422bc61b54a676234d17fc",
+        author: "Robert C. Martin",
+        likes: null,
+        __v: 0
+    } 
+
+    await api
+        .post("/ai/blogs")
+        .send(testblog3)
+        .expect(400)
+    
+})
+*/
 
 afterAll(() => {
   mongoose.connection.close()
