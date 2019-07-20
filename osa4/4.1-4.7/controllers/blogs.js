2010479437ada578512db4f5a1d2e2037be3c53a
Tenukki <tenhunens@hotmail.com>
2019-07-20 13:58:36 +0300

    terkdaw

--- osa4/4.1-4.7/controllers/blogs.js
+++ osa4/4.1-4.7/controllers/blogs.js
@@ -26,37 +26,49 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs.map(blog => blog.toJSON()))
 })
 
+blogsRouter.delete('/:id', async (request, response,next) => {
+    const r = await Blog.findById(request.params.id)
+    console.log(r)
+    if(!r){
+      response.status(404).end()
+    }else{
+      try {
+        await Blog.findByIdAndDelete(request.params.id)
+        response.status(204).end()
+      } catch (error) {
+        next(error)
+      }
+      
+    }
+});
+
 blogsRouter.post('/', async (request, response) => {
   
     try {
       if(!request.body.hasOwnProperty('url') && !request.body.hasOwnProperty('title')){
-        response.status(400)
+          response.status(400).end()
       }else{
+        if(!request.body.hasOwnProperty('likes')){
+          console.log(request.body)
+          let a = request.body
+          let uusi = {
+            title: a.title,
+            author: a.author,
+            url: a.url,
+            likes: 0
+          }
+          console.log(uusi)
 
-      
-
-      if(!request.body.hasOwnProperty('likes')){
-        console.log(request.body)
-
-        let a = request.body
-        let uusi = {
-          title: a.title,
-          author: a.author,
-          url: a.url,
-          likes: 0
+          const newBlog = new Blog(uusi)
+          newBlog.save()
+          response.json(uusi)
+          }else{
+              console.log("täällä jossa on likes")
+              const blog = new Blog(request.body)
+              const savedblog = await blog.save()
+              response.json(savedblog.toJSON())
+          }
         }
-        console.log(uusi)
-        
-        const newBlog = new Blog(uusi)
-        newBlog.save()
-        response.json(uusi)
-        }else{
-            console.log("täällä jossa on likes")
-            const blog = new Blog(request.body)
-            const savedblog = await blog.save()
-            response.json(savedblog.toJSON())
-        }
-    }
     } catch (error) {
       next(error)
     }
